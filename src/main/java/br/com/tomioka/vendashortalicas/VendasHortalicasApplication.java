39a78a0c2dbc4f72602a1df42b128dfca51d92cc
Henrique Yudi Tomioka <henriqueyudi2112@gmail.com>
2021-03-24 12:27:29 -0300

    Criacao do projeto

---
+++ src/main/java/br/com/tomioka/vendashortalicas/VendasHortalicasApplication.java
@@ -0,0 +1,13 @@
+package br.com.tomioka.vendashortalicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasHortalicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendasHortalicasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tomioka/vendashortalicas/VendasHortalicasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tomioka.vendashortalicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasHortalicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
